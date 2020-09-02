7096be12a8ab4d41f99cf1e436dbf1dbbff55279
aso <titechei@gmail.com>
2020-09-02 11:59:59 +0900

    get MAC Address

--- getip.go
+++ getip.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 
-	ip4, ip6, err := externalIP2()
+	ip4, ip6, mac, err := externalIP3()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(ip4, ip6)
+	fmt.Println(ip4, ip6, mac)
 
 }
 
@@ -98,5 +98,47 @@ func externalIP2() (string, string, error) {
         return "", "", errors.New("are you connected to the network?")
     }else{
         return ipv4Addr.String(), ipv6Addr.String(), nil
+    }
+}
+
+func externalIP3() (string, string, string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", "", "", err
+	}
+    var ipv4Addr net.IP
+    var ipv6Addr net.IP
+		var mac string
+	for _, iface := range ifaces {
+        if iface.Name != "en0" {
+            continue // select device "eth0" or something
+        }
+		mac = iface.HardwareAddr.String()
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", "", "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			if ipv4 := ip.To4(); ipv4 != nil {
+				ipv4Addr = ipv4
+			}else if ipv6 := ip.To16(); ipv6 != nil {
+				ipv6Addr = ipv6
+			}
+		}
+    }
+    if ipv4Addr == nil{
+        return "", "", "", errors.New("are you connected to the network?")
+    }else{
+        return ipv4Addr.String(), ipv6Addr.String(), mac, nil
     }
 }
